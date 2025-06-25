c5133c44c4dfaaf71a25a7cf692650b4568e6988
javier <javi_mr@me.com>
2025-06-25 13:00:35 +0200

    feat: added diagnostics dialog

--- init.lua
+++ init.lua
@@ -452,6 +452,11 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      -- Floating window for diagnostics
+      vim.keymap.set('n', '<leader>e', function()
+        vim.diagnostic.open_float(nil, { scope = 'line' })
+      end, { desc = 'Show line diagnostics' })
+
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
